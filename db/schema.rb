1c3dafac437944d1806fbb752db0695ee723ccce
André Rossini Bacchi <andre.rossini.bacchi@gmail.com>
2020-11-07 19:02:35 -0300

    Create users

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_07_215649) do
+ActiveRecord::Schema.define(version: 2020_11_07_220143) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
+
+  create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
